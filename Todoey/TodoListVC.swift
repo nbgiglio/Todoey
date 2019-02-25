fb79d79f1c2b86ad17059f38b5ce27a18f4229f5
Nick Giglio <nick@nicks-macbook-pro.local>
2019-02-24 19:47:14 -0500

    Todo list complete with datasource and delegate methods

---
+++ Todoey/TodoListVC.swift
@@ -0,0 +1,61 @@
+//
+//  TodoListVC.swift
+//  Todoey
+//
+//  Created by Nick Giglio on 2/24/19.
+//  Copyright © 2019 Nick Giglio. All rights reserved.
+//
+
+import UIKit
+
+
+class TodoListVC: UITableViewController {
+    
+    let itemArray = ["Find Mike","Buy Eggos", "Destroy Demogorgon"]
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType != .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+        
+    }
+
+}
